1b4f592e19785002784e6eebb27966bb7ad5e95a
hangry-bird <starcjs7@gmail.com>
2022-03-09 14:35:45 +0900

    webpack config change

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  // entry: "./src/index.js",
+  entry: path.resolve(__dirname, './src/index.js'),
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "bundle.js",
